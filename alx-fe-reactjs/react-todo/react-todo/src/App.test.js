15b579d35a1bcc3b9d7d6e40fa4feef6dd85e8f2
mauyaa <mauyabevan@gmail.com>
2025-10-12 22:25:16 +0300

    Delete alx-fe-reactjs directory

--- alx-fe-reactjs/react-todo/react-todo/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders the todo list heading', () => {
-  render(<App />);
-  expect(
-    screen.getByRole('heading', { name: /todo list/i })
-  ).toBeInTheDocument();
-});
